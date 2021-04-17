2d50a940fab355e42eed10be9b37459533c352a0
IrvanMahardhika <irvan.mahardhika@icloud.com>
2021-04-17 14:15:34 +0700

    Fix dbConfig.js for heroku production.

--- dist/config/dbConfig.js
+++ dist/config/dbConfig.js
@@ -1,10 +1,17 @@
 "use strict";
 module.exports = {
     development: {
-        username: process.env.DB_USERNAME || 'idsirvan',
-        password: process.env.DB_PASSWORD || 'password',
-        database: process.env.DB_NAME || 'idstaskmanager',
-        host: process.env.HOST || 'db4free.net',
+        username: 'idsirvan',
+        password: 'password',
+        database: 'idstaskmanager',
+        host: 'db4free.net',
+        dialect: 'mysql',
+    },
+    production: {
+        username: 'idsirvan',
+        password: 'password',
+        database: 'idstaskmanager',
+        host: 'db4free.net',
         dialect: 'mysql',
     },
 };
